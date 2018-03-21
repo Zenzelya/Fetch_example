809559cc9cfcee3bd5136741107d7441096f5834
A#Kelhankin <avkelgankin@yandex.ru>
2018-03-21 15:30:44 +0200

    Fetch_Api

--- script.js
+++ script.js
@@ -7,45 +7,30 @@ http://fecore.net.ua/rest/?action=2&id=1&name=Hey1&score=13 -изменение
 http://fecore.net.ua/rest/?action=1&name=Mark&score=100  - добавление
 http://fecore.net.ua/rest/
 
-  Написать функцию post, которая используя
-  REST сервис по адресу http://fecore.net.ua/rest/
-  посылает post запрос с именем введенным в input.
-  
-  Результатом fetch будет ответ от сервера со статусом
-  операции записи, вывести ОК или ERROR в поле result.
 */
 
-const input = document.querySelector("input");
-const postBtn = document.querySelector("#js-post");
-const result = document.querySelector(".result");
-const getusers = document.querySelector("#getusers");
+const add_name = document.querySelector("#addname"),
+      add_score = document.querySelector("#addscore"),
+
+      remove_user_id = document.querySelector("#removeUserid"),
+      remove_user = document.querySelector('#removeUser'),
+
+      id_updateUser = document.querySelector("#id-changeUser"),
+      name_updateUser = document.querySelector("#name-changeUser"),
+      score_updateUser = document.querySelector("#score-changeUser"),
+      btn_updateuser = document.querySelector('#changeUser'),
+
+      result = document.querySelector(".result"),
+      getusers = document.querySelector("#getusers"),
+      add_User = document.querySelector("#addUser");
 
 
-
-/*
-  @param {FormEvent} evt
-*/
 
 getusers.addEventListener("click", getUsers);
+add_User.addEventListener("click", addUser);
+remove_user.addEventListener("click", removeUser);
+btn_updateuser.addEventListener("click", updateUser);
 
-
-/*function getUsers(event){
-  event.preventDefault();
-  let url = "http://fecore.net.ua/rest/";
-
-  fetch(url)
-    .then(response => {
-      if (response.ok) return response.json();
-      throw new Error("Error fetching data");
-    })
-    .then(data =>{
-      console.log(data);
-      result.textContent = JSON.stringify(data);
-    }) 
-    .catch(error => {
-      console.error("Error: ", error);
-    });
-}*/
 
 function getUsers(event) {
   event.preventDefault();
@@ -86,3 +71,47 @@ function getUsers(event) {
     });
        
 }
+
+function addUser(event){
+  event.preventDefault();
+  const url =`http://fecore.net.ua/rest/?action=1&name=${add_name.value}&score=${add_score.value}`;
+
+  fetch(url)
+        .then(response => {
+          if (response.ok) return response.json();
+          throw new Error("Error fetching data");
+      })
+        .catch(error => {
+          console.error("Error: ", error);
+    });
+
+}
+
+function removeUser(event){
+  event.preventDefault();
+  const url = `http://fecore.net.ua/rest/?action=3&id=${remove_user_id.value}`;
+
+  fetch(url)
+        .then(response => {
+          if (response.ok) return response.json();
+          throw new Error("Error fetching data");
+      })
+        .catch(error => {
+          console.error("Error: ", error);
+    });
+}
+
+
+function updateUser(event){
+  event.preventDefault();
+  const url = `http://fecore.net.ua/rest/?action=2&id=${id_updateUser.value}&name=${name_updateUser.value}&score=${score_updateUser.value}`;
+
+  fetch(url)
+          .then(response => {
+            if (response.ok) return response.json();
+            throw new Error("Error fetching data");
+        })
+          .catch(error => {
+            console.error("Error: ", error);
+      });
+  }
